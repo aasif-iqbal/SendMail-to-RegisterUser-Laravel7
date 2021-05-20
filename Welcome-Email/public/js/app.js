f2589b31e22dacf16480678cf89fc61ca85b13dc
aasif-iqbal <aasif.iqbal3000@gmail.com>
2021-05-20 11:37:49 +0530

    screensort add

--- Welcome-Email/public/js/app.js
+++ Welcome-Email/public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/Welcome-Email/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/Welcome-Email/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/Github/SendMail-to-RegisterUser-Laravel7/Welcome-Email/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/Github/SendMail-to-RegisterUser-Laravel7/Welcome-Email/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
